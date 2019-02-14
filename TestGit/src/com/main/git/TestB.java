fbd15dc0a72a249c9f3b2fff6f95aa1f6bd1481e
kavyasolipuram <kavya.0913@gmail.com>
2019-02-14 11:39:52 -0500

    Added if else statement in TestB.class.

--- TestGit/src/com/main/git/TestB.java
+++ TestGit/src/com/main/git/TestB.java
@@ -3,8 +3,10 @@ package com.main.git;
 public class TestB {
 
 	public String displayName(String name) {
-		
-		return "Your name is " + name;
+		if (name == "Kavya")
+			return "Your name is " + name;
+		else 
+			return "Your name is Shourya";
 	}
 	
 }
