8a4e04968c929fb25569cff838207bb3370889b5
kavyasolipuram <47532314+kavyasolipuram@users.noreply.github.com>
2019-02-14 11:50:31 -0500

    Added Reddy in sysout in TestA.class

--- TestGit/src/com/main/git/TestA.java
+++ TestGit/src/com/main/git/TestA.java
@@ -5,7 +5,7 @@ public class TestA {
 	public static void main(String[] args) {
 		TestB test = new TestB();
 		System.out.println(test.displayName("Kavya"));
-			
+		System.out.println(test.displayName(" Reddy"));	
 	}
 
 }
