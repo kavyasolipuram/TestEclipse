951bdc9463fb98e9b61251224f1db9dbc55cbb94
kavyasolipuram <47532314+kavyasolipuram@users.noreply.github.com>
2019-02-14 15:37:29 -0500

    Removed

--- TestGit/src/com/main/git/TestFinal.java
+++ TestGit/src/com/main/git/TestFinal.java
@@ -4,9 +4,6 @@ public class TestFinal {
 
     void test() {
       System.out.println("Final Test");
-      System.out.println("Done");
-      System.out.println("Not done");
-      System.out.println("1");
     }
 
 }
