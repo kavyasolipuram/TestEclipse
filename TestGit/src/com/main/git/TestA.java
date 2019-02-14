2147fa5613c761190c14e3ce6d4b287d09590393
kavyasolipuram <kavya.0913@gmail.com>
2019-02-14 12:15:19 -0500

    Deleted sysout with Solipuram in TestA.class.

--- TestGit/src/com/main/git/TestA.java
+++ TestGit/src/com/main/git/TestA.java
@@ -4,9 +4,7 @@ public class TestA {
 
 	public static void main(String[] args) {
 		TestB test = new TestB();
-		System.out.println(test.displayName("Kavya"));
-		System.out.println(" Solipuram");
-			
+		System.out.print(test.displayName("Kavya"));
 		System.out.println(test.displayName(" Reddy"));	
 	}
 
